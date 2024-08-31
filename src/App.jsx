7ae2ae2f89777e904b5b4fe7fe1eb7584f85e00d
Yashraj <59451283+wolffkishner@users.noreply.github.com>
2024-09-01 01:11:28 +0530

    button with visualiser

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,63 @@
-import './App.css'
+import { useState } from "react";
+import { FaMicrophoneAlt, FaMicrophoneAltSlash } from "react-icons/fa";
+import "./App.css";
+import { LiveAudioVisualizer } from "react-audio-visualize";
 
 function App() {
+  const [micinput, setMicinput] = useState(false);
+
+  const [mediaInput, setMediaInput] = useState();
+
+  const handleClick = () => {
+    if (micinput) {
+      setMicinput(!micinput);
+      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.stop();
+        setMediaInput(null);
+      });
+      setMediaInput();
+    } else {
+      setMicinput(!micinput);
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          const mediaRecorder = new MediaRecorder(stream);
+          mediaRecorder.start();
+          setMediaInput(mediaRecorder);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <>
-      <div>Hello</div>
+      <div className="flex flex-col min-h-screen items-center justify-center">
+        <button onClick={handleClick}>
+          <FaMicrophoneAlt
+            className={`rounded-full shadow-md  ${
+              !micinput && `hover:shadow-emerald-500`
+            } border p-10 transition-all duration-200 ${
+              micinput &&
+              `shadow-gray-500 transition-all duration-100 hover:shadow-red-700`
+            }`}
+            size={166}
+          />
+        </button>
+
+        <div className={`${!mediaInput && `hidden`} absolute bottom-16`}>
+          {mediaInput && (
+            <>
+              <LiveAudioVisualizer
+                mediaRecorder={mediaInput}
+                width={200}
+                height={75}
+              />
+            </>
+          )}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root")).render(
     <link
       rel="preconnect"
       href="https://fonts.gstatic.com"
-      crossOrigin={true}
+      crossOrigin='anonymous'
     ></link>
     <link
       href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
