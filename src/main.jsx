f831266c87f3027d317afbfc38a5070e811b487e
Yashraj <59451283+wolffkishner@users.noreply.github.com>
2024-09-01 00:00:10 +0530

    setup basic app

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin={true}
+    ></link>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+      rel="stylesheet"
+    ></link>
+    <App />
+  </>
+);
